b736d50f4dfb29ec6f29349eb34c6aff8a53629f
Allison Seigler <55610738+allisonseigler@users.noreply.github.com>
2022-09-22 17:29:37 -0500

    attempt 1

--- memA.sv
+++ memA.sv
@@ -10,6 +10,15 @@ module memA
   output signed [BITS_AB-1:0] Aout [DIM-1:0]
 );
 
+  logic signed [BITS_AB-1:0] A_flipped [DIM-1:0];
+  
+genvar i;
+generate
+  for (i = 0; i < DIM; i++) begin
+    A_flipped[i] = Ain[DIM-i];
+    memAfifo #(.DEPTH(DIM+i), .BITS(BITS_AB)) MEM_A_FIFO(.clk(clk), .rst_n(rst_n), .en(en), .d(A_flipped), .q(Aout));
+  end
+endgenerate
 
 
 endmodule
